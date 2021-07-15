e1481217ff5fd0257135a808005cffd63b610049
Manish <Manish@LAPTOP-BDVGEQAL>
2021-07-15 16:08:17 +0530

    Array Rotation Program Basic

--- ArrayRotations/ArrayRotationProgram/Program.cs
+++ ArrayRotations/ArrayRotationProgram/Program.cs
@@ -10,7 +10,29 @@ namespace ArrayRotationProgram
     {
         public static void Main(string[] args)
         {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
 
+            LeftRotateOneByOne.LeftRotate(arr, 2, 7);
+            
+            PrintArray(arr, 7);
+
+            Console.WriteLine("\nArray Rotated using juggling Algorithm");
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+            LeftRotateJugglingAlgorithm.LeftRotate(arr1, 2, 7);
+
+            PrintArray(arr1, 7);
+            Console.Read();
+        }
+
+        
+
+        /* utility function to print an array */
+        static void PrintArray(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(arr[i] + " ");
+
+            
         }
     }
 }
